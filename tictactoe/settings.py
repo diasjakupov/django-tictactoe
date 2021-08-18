943d400b7f72f87e4f5fd29606d0abd3635a00a9
diasjakupov <dsjakupov@gmail.com>
2021-08-18 15:22:40 +0600

    setup django channels

--- tictactoe/settings.py
+++ tictactoe/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'tictactoe.wsgi.application'
+ASGI_APPLICATION = "tictactoe.asgi.application"
+
 
 
 # Database
